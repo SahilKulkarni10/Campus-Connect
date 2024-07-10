03c4cdf36f213e98c23889e063c405ace4d50fb6
Sahil kulkarni <121624855+SahilKulkarni10@users.noreply.github.com>
2024-07-10 23:58:46 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -10,7 +10,12 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.ve/cel.app", process.env.CLIENT_URL],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
