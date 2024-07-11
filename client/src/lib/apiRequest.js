608d5c17a2754eae0063e93fe4bade16c3aae06e
Sahil kulkarni <121624855+SahilKulkarni10@users.noreply.github.com>
2024-07-11 13:11:13 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://campus-connect-api-ruddy.vercel.app",
   withCredentials: true,
 });
 
